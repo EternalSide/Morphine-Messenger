e544624f35980f00246efffb0a5d05d9aaf7b99e
alex <reidsgadv@gmail.com>
2023-06-13 19:59:23 +1000

    Сохранился

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,6 +1,8 @@
 "use client";
 type Variant = "LOGIN" | "REGISTER";
-
+import AuthSocialButton from "../AuthSocialButton";
+import { BsGithub } from "react-icons/bs";
+import { BsGoogle } from "react-icons/bs";
 import { useState, useCallback } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 import Input from "@/app/components/inputs/Input";
@@ -12,10 +14,11 @@ const AuthForm = () => {
   const toogleVariant = useCallback(() => {
     if (variant === "LOGIN") {
       setVariant("REGISTER");
+      console.log(variant);
     } else {
       setVariant("LOGIN");
     }
-  }, []);
+  }, [variant]);
 
   const {
     register,
@@ -47,13 +50,35 @@ const AuthForm = () => {
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
       <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
         <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-          {variant === "REGISTER" && <Input id="name" label="Name" register={register} errors={errors} />}
+          {variant === "REGISTER" && <Input id="name" label="Name" register={register} disabled={isLoading} errors={errors} />}
           <Input id="email" label="Email" type="email" register={register} errors={errors} />
           <Input id="password" label="Пароль" type="password" register={register} errors={errors} />
           <div>
-            <Button>Test</Button>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "REGISTER" ? "Регистрация" : "Войти"}
+            </Button>
           </div>
         </form>
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm ">
+              <span className="bg-white px-2 text-gray-500">Или войдите с помощью</span>
+            </div>
+          </div>
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton icon={BsGithub} onClick={() => socialAction("github")} />
+            <AuthSocialButton icon={BsGoogle} onClick={() => socialAction("google")} />
+          </div>
+        </div>
+        <div className="flex gap-2 justify-center text-sm mt-6 px-2 text-gray-500">
+          <div>{variant === "LOGIN" ? "Впервые на NextChat?" : "Уже есть аккаунт?"}</div>
+          <div className="underline cursor-pointer" onClick={toogleVariant}>
+            {variant === "LOGIN" ? "Создать аккаунт" : "Войти"}
+          </div>
+        </div>
       </div>
     </div>
   );
