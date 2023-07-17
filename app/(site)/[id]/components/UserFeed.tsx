76f26225f04e55421b0df51abe555edd0e7fa623
alex <reidsgadv@gmail.com>
2023-07-17 17:23:28 +1000

    fix

--- app/(site)/[id]/components/UserFeed.tsx
+++ app/(site)/[id]/components/UserFeed.tsx
@@ -5,16 +5,9 @@ interface UserFeedProps {
   user: User;
 }
 const UserFeed: React.FC<UserFeedProps> = ({ user }) => {
-  if (!user?.posts) {
-    return (
-      <div className="flex flex-col  justify-center  lg:w-2/4 sm:w-3/4">
-        <h2 className="text-neutral-500 font-bold text-center text-xl mt-6">Информация отсуствует.</h2>
-      </div>
-    );
-  }
   return (
-    <div className="flex flex-col  justify-center border-[1px] border-t-[0] border-neutral-800 lg:w-2/4 sm:w-3/4 overflow-y-auto">
-      <UserPost userImage={user.image!} username={user.name!} />
+    <div className="flex flex-col  justify-center  lg:w-2/4 sm:w-3/4">
+      <h2 className="text-neutral-500 font-bold text-center text-xl mt-6">Информация отсуствует.</h2>
     </div>
   );
 };
--- app/(site)/[id]/page.tsx
+++ app/(site)/[id]/page.tsx
@@ -1,5 +1,4 @@
 import UserProfile from "./components/UserProfile";
-import Sidebar from "@/app/components/sidebar/Sidebar";
 
 const userProfile = ({ params }: { params: { id: string } }) => {
   return <UserProfile id={params.id} />;
