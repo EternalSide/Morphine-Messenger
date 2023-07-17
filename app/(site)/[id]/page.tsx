1d2e4d83eca73d46cdf679cb926b36b0860ef178
alex <reidsgadv@gmail.com>
2023-07-17 17:20:00 +1000

    fix import

--- app/(site)/[id]/page.tsx
+++ app/(site)/[id]/page.tsx
@@ -1,7 +1,8 @@
 import UserProfile from "./components/UserProfile";
 import Sidebar from "@/app/components/sidebar/Sidebar";
 
-export const userProfile = ({ params }: { params: { id: string } }) => {
+const userProfile = ({ params }: { params: { id: string } }) => {
   return <UserProfile id={params.id} />;
 };
+
 export default userProfile;
